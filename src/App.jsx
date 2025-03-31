0f166860d9f271f5284342d56a2ca90167f2f751
Dawid Winogrodzki <dawid.winogrodzki@tkkom.local>
2025-03-31 16:10:24 +0200

    connected to api and simple layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+//Color Pattern https://colorhunt.co/palette/443627d98324efdcabf2f6d0
 
+const options = {
+    apikey: "ebe9a0bc",
+};
 function App() {
-  const [count, setCount] = useState(0)
+    const [movieData, setMovieData] = useState([]);
+    const [search, setSearch] = useState("");
+    useEffect(() => {
+        fetch(
+            `http://www.omdbapi.com/?apikey=${options.apikey}&s=${search}&plot=full`
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setMovieData(res.Search);
+            })
+            .catch((err) => console.error(err));
+    }, [search]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Container className="body">
+            <Row>
+                <Col>
+                    <input
+                        className="input"
+                        value={search}
+                        onInput={(e) => setSearch(e.target.value)}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h1>Welcome to MovieRental!!!</h1>
+                    {movieData ? (
+                        movieData.map((e) => (
+                            <>
+                                <h3>{e.Title}</h3>
+                                <a
+                                    target="_blank"
+                                    href={`https://www.imdb.com/title/${e.imdbID}`}
+                                >
+                                    <img className="poster" src={e.Poster} />
+                                </a>
+                            </>
+                        ))
+                    ) : (
+                        <h1 className="text-danger">No movies found</h1>
+                    )}
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
-export default App
+export default App;
